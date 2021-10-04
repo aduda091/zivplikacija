30895fb3e308e0fc4d31fe349f270dcb01204361
Alen Duda <alenduda@gmail.com>
2021-10-04 23:29:03 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const app = new Vue({
+    el: "#app",
+    data() {
+        return {
+            soundClips: [
+                {
+                    artist: "Lepi",
+                    title: "Iznad oblaka",
+                    image: "https://img.youtube.com/vi/EdUbFl93TlY/hqdefault.jpg",
+                    audio: ""
+                },
+                {
+                    artist: "Lepi",
+                    title: "Pusti me",
+                    image: "https://img.youtube.com/vi/EdUbFl93TlY/hqdefault.jpg",
+                    audio: ""
+                },
+                {
+                    artist: "Lepi",
+                    title: "Skloni mi se s puta mala",
+                    image: "https://img.youtube.com/vi/EdUbFl93TlY/hqdefault.jpg",
+                    audio: ""
+                },
+                {
+                    artist: "Lepi",
+                    title: "Iznad oblaka",
+                    image: "https://img.youtube.com/vi/EdUbFl93TlY/hqdefault.jpg",
+                    audio: ""
+                },
+                {
+                    artist: "Hako obić",
+                    title: "Ko na ćilimu",
+                    image: "https://img.youtube.com/vi/vwMUYFiefvk/hqdefault.jpg",
+                    audio: ""
+                },
+                {
+                    artist: "Elettra Lamborghini",
+                    title: "Pistolero",
+                    image: "https://img.youtube.com/vi/H1HlUe886Hs/hqdefault.jpg",
+                    audio: ""
+                },
+            ]
+        };
+    }
+});
