1e0cd46909f41894f7eb1308747c4f36e77b159f
Alen Duda <alenduda@gmail.com>
2021-10-05 00:34:49 +0200

    audio player

--- script.js
+++ script.js
@@ -5,41 +5,52 @@ const app = new Vue({
             soundClips: [
                 {
                     artist: "Lepi",
-                    title: "Iznad oblaka",
+                    title: "Iza oblaka",
                     image: "https://img.youtube.com/vi/EdUbFl93TlY/hqdefault.jpg",
-                    audio: ""
+                    audio: "izaOblaka.mp3"
                 },
-                {
-                    artist: "Lepi",
-                    title: "Pusti me",
-                    image: "https://img.youtube.com/vi/EdUbFl93TlY/hqdefault.jpg",
-                    audio: ""
-                },
+
                 {
                     artist: "Lepi",
                     title: "Skloni mi se s puta mala",
                     image: "https://img.youtube.com/vi/EdUbFl93TlY/hqdefault.jpg",
-                    audio: ""
-                },
-                {
-                    artist: "Lepi",
-                    title: "Iznad oblaka",
-                    image: "https://img.youtube.com/vi/EdUbFl93TlY/hqdefault.jpg",
-                    audio: ""
+                    audio: "mala.mp3"
                 },
                 {
                     artist: "Hako obić",
                     title: "Ko na ćilimu",
                     image: "https://img.youtube.com/vi/vwMUYFiefvk/hqdefault.jpg",
-                    audio: ""
+                    audio: "cilim.mp3"
+                },
+                {
+                    artist: "Efendi",
+                    title: "Mata Hari",
+                    image: "https://img.youtube.com/vi/9k24ab_u6gs/hqdefault.jpg",
+                    audio: "mataHari.mp3"
                 },
                 {
                     artist: "Elettra Lamborghini",
                     title: "Pistolero",
                     image: "https://img.youtube.com/vi/H1HlUe886Hs/hqdefault.jpg",
-                    audio: ""
-                },
-            ]
+                    audio: "pistolero.mp3"
+                }
+            ],
+            currentAudio: null,
+            isPlaying: false
         };
+    },
+    methods: {
+        playItem(clip) {
+            if (clip.audio === this.currentAudio) {
+                this.$refs.audioPlayer.pause();
+                this.isPlaying = false;
+                this.currentAudio = null;
+                return;
+            }
+            this.currentAudio = clip.audio;
+            this.isPlaying = true;
+            this.$refs.audioPlayer.load();
+            this.$refs.audioPlayer.play();
+        }
     }
 });
